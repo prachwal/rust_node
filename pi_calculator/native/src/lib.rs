2e11a326a93e47ce9dd827f0bd7d9f5d50df9ddd
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2025-03-16 22:35:53 +0100

    neon update to 1.0.0

--- pi_calculator/native/src/lib.rs
+++ pi_calculator/native/src/lib.rs
@@ -4,23 +4,23 @@ use std::f64;
 fn calculate_pi(iterations: i32) -> f64 {
     let mut pi = 0.0;
     let mut sign = 1.0;
-    
+
     for i in 0..iterations {
         pi += sign * 4.0 / (2.0 * i as f64 + 1.0);
         sign = -sign;
     }
-    
+
     pi
 }
 
 fn calculate_pi_neon(mut cx: FunctionContext) -> JsResult<JsNumber> {
-    let iterations = cx.argument::<JsNumber>(0)?.value() as i32;
+    let iterations = cx.argument::<JsNumber>(0)?.value(&mut cx) as i32;
     let pi = calculate_pi(iterations);
     Ok(cx.number(pi))
 }
 
-// Poprawna rejestracja modułu w Neon
-register_module!(mut cx, {
+#[neon::main]
+fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("calculatePi", calculate_pi_neon)?;
     Ok(())
-});
+}
